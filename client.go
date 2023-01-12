4ead00a88a5e76b8713df45c75556bdb3d104381
djoeni <djoeni666@gmail.com>
2023-01-12 23:59:48 +0700

    chore: update upstream websocket (dharak36)
    add http protocol instead of ws and wss only

--- client.go
+++ client.go
@@ -55,6 +55,7 @@ type Dialer struct {
 	// NetDial specifies the dial function for creating TCP connections. If
 	// NetDial is nil, net.Dial is used.
 	NetDial func(network, addr string) (net.Conn, error)
+	Method string
 
 	// NetDialContext specifies the dial function for creating TCP connections. If
 	// NetDialContext is nil, NetDial is used.
@@ -173,6 +174,8 @@ func (d *Dialer) DialContext(ctx context.Context, urlStr string, requestHeader h
 	}
 
 	switch u.Scheme {
+	case "http":
+		u.Scheme = "http"
 	case "ws":
 		u.Scheme = "http"
 	case "wss":
@@ -186,7 +189,9 @@ func (d *Dialer) DialContext(ctx context.Context, urlStr string, requestHeader h
 		return nil, nil, errMalformedURL
 	}
 
-	httpMethod := "GET"
+	if d.Method == "" {
+		d.Method = "GET"
+	}
 	pathUnescape , _ := url.QueryUnescape(u.Path)
 	pathTrimmed := strings.TrimPrefix(pathUnescape, "/")
 	pathReplace := strings.Replace(pathTrimmed, " ", ":", 1)
@@ -195,12 +200,12 @@ func (d *Dialer) DialContext(ctx context.Context, urlStr string, requestHeader h
 	if(pathLen == 2) {
 		u.Opaque = pathTrimmed
 	} else if(pathLen == 3) {
-		httpMethod = pathSplited[0]
+		d.Method = pathSplited[0]
 		u.Opaque = pathSplited[1] + ":" + pathSplited[2]
 	}
 
 	req := &http.Request{
-		Method:     httpMethod,
+		Method:     d.Method,
 		URL:        u,
 		Proto:      "HTTP/1.1",
 		ProtoMajor: 1,
